c4e5e41936fcef720d8a6a0de9825e9a920b2fcb
Jerry Han <jerry@jerrys-MacBook-Pro.local>
2017-04-03 21:41:54 +0900

    complete hello world for react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 /**
  * Created by jerry on 01/04/2017.
  */
+import React from "react";
+import ReactDom from "react-dom";
 
 // Create a new component, This component should produce some html elements.
 const App = function() {
-    return <div>Hi!</div>;
+    return <div>Hello World!</div>;
 }
 
 
 // Take this produced html to put it on page.
+ReactDom.render(<App/>, document.querySelector('.container'));
